33b3431e50be2ddb5f756da60253fcefe2efb846
Shaban Dibra <39242502+shabandibra@users.noreply.github.com>
2022-06-24 17:26:16 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,3 @@
-import type { NextRequest, NextResponse } from 'next/server'
-import { areCredentialsValid } from '../lib'
-
-export function middleware(req: NextRequest) {
-  if (areCredentialsValid(req.headers.get('authorization'))) {
-    return NextResponse.next()
-  }
-  return NextResponse.redirect(
-    new URL(`/login?from=${req.nextUrl.pathname}`, req.url)
-  )
-}
-
-
 
 import type { NextRequest, NextResponse } from 'next/server';
 
